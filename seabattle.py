346e134def1af50c8d9235110c45732112764a86
angelaangelaa <demanenkoanzela640@gmail.com>
2024-11-23 17:02:57 +0600

    Update seabattle.py

--- seabattle.py
+++ seabattle.py
@@ -10,3 +10,6 @@ sunk = "#"
 
 board = [[empty] * board_size for _ in range(board_size)]
 player_board = [[empty] * board_size for _ in range(board_size)]
+
+ships = []
+ship_sizes = [3, 2, 2, 1, 1, 1, 1]
