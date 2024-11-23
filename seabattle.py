c879402ee7052ccd87547f88348865d33ddda637
angelaangelaa <demanenkoanzela640@gmail.com>
2024-11-23 14:23:56 +0600

    Update seabattle.py
    creating two empty boards

--- seabattle.py
+++ seabattle.py
@@ -1,2 +1,12 @@
+
 import os
 import random
+
+board_size = 7
+empty = "~"  
+miss = "O"   
+hit = "X"   
+sunk = "#"  
+
+board = [[empty] * board_size for _ in range(board_size)]
+player_board = [[empty] * board_size for _ in range(board_size)]
